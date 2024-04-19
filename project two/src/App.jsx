62ba32d34f530fc4891791723ea4c435a1e4e91a
torrento <sachinles99@gmail.com>
2024-04-19 19:05:01 +0530

    added

--- project two/src/App.jsx
+++ project two/src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Card from "./components/card"
+import Card from "./components/Card"
 
 function App() {
   const [count, setCount] = useState(0);
 
+  let myobj = {
+    name: "nitin",
+    age: 24
+  }
+
+
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 mb-4">tailwind</h1>
-      <Card />
-      <Card />
+      <Card username="chai aur code" getobj={myobj }/>
+      <Card username="props use" getobj={myobj}/>
     </>
   );
 }
