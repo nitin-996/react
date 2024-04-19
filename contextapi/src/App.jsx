1ddf9d723a90b272d8355d16b09f952c868e522d
nitin <nitin@localhost.localdomain>
2024-04-19 22:54:44 +0530

    added

--- contextapi/src/App.jsx
+++ contextapi/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { createContext } from 'react'
 import Component3 from './Components/Component3';
+import Pokemon from './Components/Pokemon';
 
   export {firstName}
 // created context
@@ -12,6 +13,8 @@ function App() {
 
   return (
     <>
+
+<Pokemon></Pokemon>
 
 {/* // context provider */}
     <firstName.Provider value={"nitin"}><Component3/></firstName.Provider>
--- contextapi/src/Components/Component3.jsx
+++ contextapi/src/Components/Component3.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { firstName } from '../App'
+
 function Component3() {
-  return 
-
-    // its consumer , it only wants function
-    <firstName.Consumer>{(fname)=>{
-
-    return <h1>my name is monoto {fname}<h1/>;
-    }}
-    </firstName.Consumer>
-   
-  
+  return (
+    <>
+      <firstName.Consumer>
+        {(fname) => <h1>My name is monoto {fname}</h1>}
+      </firstName.Consumer>
+    </>
+  );
 }
 
 export default Component3
