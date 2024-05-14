fc9e8112230c51ab743629b3c1bba00dc4130590
torrento <sachinles99@gmail.com>
2024-05-14 18:59:59 +0530

    added

--- academind tik-toe/Components/Players.jsx
+++ academind tik-toe/Components/Players.jsx
@@ -1,31 +1,35 @@
 import { useState } from "react";
 import React from "react";
 
-function Players({ name, symbol }) {
+function Players({ name, symbol , isActive }) {
   const [Edit, setEdit] = useState(false);
-  const [value , setValue] = useState(name);
+  const [value, setValue] = useState(name);
 
   let changeinput = <span className="player-name">{value}</span>;
 
-const getValue = (e)=>{
+  const getValue = (e) => {
     const even = e.target.value;
-    setValue(even) 
+    setValue(even);
+  };
 
-}
+  const Save = () => setEdit((prevalue) => !prevalue);
 
-
-  const Save = () => setEdit((prevalue)=> !prevalue);
-
-  let valueinner = "Edit"
+  let valueinner = "Edit";
   if (Edit) {
-    changeinput = <input className="player-name" type="text" required value={value} onChange={getValue}/>;
-valueinner = "save"
-
+    changeinput = (
+      <input
+        className="player-name"
+        type="text"
+        required
+        value={value}
+        onChange={getValue}
+      />
+    );
+    valueinner = "save";
   }
 
-
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {changeinput}
         <span className="player-symbol">{symbol}</span>
@@ -35,7 +39,7 @@ valueinner = "save"
       {/* 1st logic */}
       {/* <button onClick={Save}>{Edit ? "save" : "Edit"}</button> */}
       {/* 2nd logic */}
-      <button onClick={Save}>{valueinner}</button>
+      <button  onClick={Save}>{valueinner}</button>
     </li>
   );
 }
--- academind tik-toe/src/App.jsx
+++ academind tik-toe/src/App.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react"
+import Gameboard from "../Components/Gameboard"
 import Players from "../Components/Players"
 
 function App() {
   
+const [activeplayer , setactiveplayer] = useState()
 
+const handleSelectsquare=()=>{
+  setactiveplayer((prevalue)=>(prevalue === "X" ? "O" : "X"))
+}
   return (
-    <main id="game-container">
+    <main >
 
-<ol>
-<li>
-  <Players name="Player 1" symbol="X"/>
-  <Players name="Player 2" symbol="O"/>
-</li>
+<div id="game-container">
+<ol id="players" className="highlight-player">
 
+  <Players name="Player 1" symbol="X" isActive={activeplayer === "X"}/>
+  <Players name="Player 2" symbol="O" isActive={activeplayer === "O"}/>
 
 </ol>
+<Gameboard onSelectExecute={handleSelectsquare } activeSymbolOnBoard={activeplayer} />
+</div>
 
     </main>
   )
