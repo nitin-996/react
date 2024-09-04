abd928fbcfc2c10c4d5bd7edcd6b8dd1158422cf
manish-github-test <30manish2004@gmail.com>
2024-09-04 23:35:22 +0530

    added

--- Quiz app/src/components/Quiz.jsx
+++ Quiz app/src/components/Quiz.jsx
@@ -1,41 +1,48 @@
 import { useState } from "react";
 import Questions from "../Questions";
+import complete from '../assets/quiz-complete.png'
+
 
 export default function Quiz() {
   // yha answers store kerna h
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuesionIndex = userAnswers?.length;
+  const activeQuestionIndex = userAnswers?.length;
+  const quizIsComplete = Questions.length === activeQuestionIndex;
 
   function handleSelectedAnswer(selectedAnswer) {
-    setUserAnswers((prevalue) => {
-      const CorrectAnswers = [...prevalue , selectedAnswer];
-      return CorrectAnswers
-      
+    setUserAnswers((prevValue) => {
+      const correctAnswers = [...prevValue, selectedAnswer];
+      return correctAnswers;
     });
   }
 
-  
-  const random = (Questions)=>Questions[activeQuesionIndex].answers.sort(()=>Math.random - 0.5)
+  if(quizIsComplete){
+    return <div id="summary">
+      <img src={complete} alt="complete quiz image" />
+      <h2>Quiz Completed</h2></div>;
+  }
   
 
-  return (
-    <div id='quiz'>
-        <div id="question">
-      <h2>{Questions[activeQuesionIndex].text}</h2>
-      <ul id="answers">
-        {random.map((answer) => 
-        
-        {
-          return (
-            <li key={answer} className="answer">
-              <button onClick={()=>handleSelectedAnswer(answer)
 
-              }>{random(answer)}</button>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-    </div>
-  );
-}
+  // Shuffle answers for the current question
+  const shuffleAnswers = [...Questions[activeQuestionIndex]?.answers];
+  shuffleAnswers.sort(() => Math.random() - 0.5);
+
+  return ( 
+            <div id="quiz">
+          <div id="question">
+            <h2>{Questions[activeQuestionIndex]?.text}</h2>
+            <ul id="answers">
+              {shuffleAnswers.map((answer) => (
+                <li key={answer} className="answer">
+                  <button onClick={() => handleSelectedAnswer(answer)}>
+                    {answer}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+
+
